3eddc574913c202aeb5ed75396ab3205b031ec4b
olgaKhristo <olga84h@gmail.com>
2023-09-15 20:50:51 +0100

    added post req

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const jokes = require('./jokes.json')
+// const jokes = require('./jokes.json')
 const port = process.env.PORT
 
-app.use(cors());
+const fs = require('fs');
+const jokes = JSON.parse(fs.readFileSync('jokes.json', 'utf8'));
+
 app.use(express.json());
+app.use(cors());
 
-const jokes = {
-    message:'Hello from moms!'
-}
 
-app.get('/', (req, res) => {
-    res.json(jokes)
+
+app.get('/jokes/random', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * jokes.length)
+    const randomJoke = jokes[randomIndex]
+    res.json({joke: randomJoke})
     // res.send(`Hello from moms!`)
+})
+app.post('/jokes', (req, res) => {
+const newJoke = req.body
+jokes.push(newJoke)
+fs.writeFileSync('jokes.json', JSON.stringify(jokes), 'utf8')
+res.sendStatus(201).send(newJoke)
 })
 
 app.get('/jokes', (req, res) => {
-    res.send(jokes)
+    res.json(jokes)
 })
 
 app.listen(port, () => {
