fdd0e89d6d1be88f92b654066e632b2b15e81935
olgaKhristo <olga84h@gmail.com>
2023-09-15 18:05:26 +0100

    adited app.get

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send(`Hello from moms!`)
+    res.json(jokes)
+    // res.send(`Hello from moms!`)
 })
 
 app.get('/jokes', (req, res) => {
